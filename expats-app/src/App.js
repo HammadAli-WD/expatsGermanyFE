1a3987cdab1933e29e83c37bffefafd6bf363703
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-09-25 23:06:08 +0200

    React Router Dom

--- expats-app/src/App.js
+++ expats-app/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from './components/Login';
-import Navbar from './components/NavBar'
-import { Row, Container } from 'react-bootstrap';
+import Home from './components/Home';
+
 function App() {
   return (
     <>
-     <Navbar />
-      <Login />   
+     <Router>
+       <Switch>
+         <Route exact path= "/" component={Login} />
+         <Route path= "/home" component={Home} />
+       </Switch>
+     </Router>   
    
     </>
   );
