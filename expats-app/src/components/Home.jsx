6cd91eeb1da26b58f460fa62cc72df1250a2fa7f
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-09-28 21:38:20 +0200

    Hooks for fetching corona cases

--- expats-app/src/components/Home.jsx
+++ expats-app/src/components/Home.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Home() {
+    const [country, setCountry] = useState("");
+    const [cases, setCases] = useState("");
+    const [date, setDate] = useState("")
+
+    useEffect( () => {
+       fetch("http://localhost:3005/covid/liveData")
+            .then( res => res.json())
+            .then(data => {console.log(data)
+            setCountry(data.Country);
+            setCases(data.Cases);
+            setDate(data.Date)
+            })
+        }, [])
     return (
-        <h1>Welcome</h1>
+        <h1> Country: {country} <br />
+             Cases: {cases}  <br />
+             Date: {date}      
+         </h1>
+
     )
+
     
 }
 
