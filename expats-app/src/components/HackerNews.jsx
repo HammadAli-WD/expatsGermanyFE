58d06786b2fd0a8621e4438c49e97c989ad21e85
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-10-01 19:12:53 +0200

    fetching data for hacker news using custim functions

--- expats-app/src/components/HackerNews.jsx
+++ expats-app/src/components/HackerNews.jsx
@@ -1,37 +1,37 @@
 import React, { useEffect, useState } from "react";
 import {Table} from 'react-bootstrap'
 import { Link } from "react-router-dom";
+import useFetch from "../HOC/UseFetch"
 
 function HackerNews() {
-    const [topNews, settopNews] = useState([]);
-   
+    
+  const [response, loading, hasError] = useFetch("http://localhost:3005/news/hackerNews") 
 
-    useEffect( () => {
-       fetch("http://localhost:3005/news/hackerNews")
-       .then(resp => resp.json())
-       .then(data => {console.log('Hacker News',data)
-       settopNews(data)
-    })
-       .catch(err => {
-        console.log(err);
-    });
-        }, [])
     return (
        <>
-       <Table striped bordered hover variant="dark">
-  <thead>
-    <tr>
-      <th>TopHackerNews</th>      
-    </tr>
-  </thead>
-  <tbody>    
-    {topNews.map((tn,i) => 
-    <tr key={i}>
-        <td> <Link to={{ pathname: tn.url }} target="_blank" >{tn.title}</Link> 
-        </td></tr>
-        )}   
-  </tbody>
-</Table>
+       
+    {loading ? 
+    <div>Loading...</div>:
+    (hasError?<div>Error...</div>:
+    (      
+    <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>TopHackerNews</th>      
+        </tr>
+      </thead>
+      <tbody>
+        {response.map((tn,i) => 
+          <tr key={i}>
+            <td> <Link to={{ pathname: tn.url }} target="_blank" >{tn.title}</Link> 
+            </td></tr>
+        )} 
+      </tbody>
+    </Table> 
+      
+    ))}    
+     
+  
 </>
 )}
 
