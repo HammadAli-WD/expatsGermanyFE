a59984ad77d874f2f1aff5abd84c47293ef554e1
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-10-21 23:43:09 +0200

    weather forecast

--- expats-app/src/components/WeatherInfo.jsx
+++ expats-app/src/components/WeatherInfo.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {Carousel, Spinner} from 'react-bootstrap'
 import useFetch from "../HOC/UseFetch"
+import ForecastItem from "./ForecastItem";
+import moment from "moment";
+import Cities from "./Cities";
 
 function WeatherInfo() {
     
   const [response, loading, hasError] = useFetch("http://localhost:3005/weather/forecast") 
-
+  const [cities, setCities] = useState([]);
+  const url = "http://localhost:3005/weather/city"
+  
+    useEffect(() => {
+     
+      fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+          //console.log('Hacker News',data)
+      setCities(data)
+     
+          
+      })
+          
+  }, [])
+    
+  
     return (
        <>
        {console.log("data", response)}
@@ -17,22 +36,40 @@ function WeatherInfo() {
     (
         
               <Carousel>
+                               
+                {cities.map((city, k) => 
+                <h1>{city.name} </h1>
+                )}
             {response.map(({lat, daily, lon}, i)=>
-            <Carousel.Item key={i}>
+            <>
             
             
             <h1>{lat}</h1>
             <h1>{lon}</h1>
+            {/* <img
+              style={{ width: 100, objectFit: "cover" }}
+              src={
+                "https://png.pngtree.com/element_our/md/20180627/md_5b33491c79ac0.jpg"
+              }
+            /> */}
           
             
-            {daily.map((fc, j) => <p key={j}>{fc.clouds}</p>)}
+            {daily.slice(0,5).map((item, j) => <ForecastItem
+             key={j}
+              day={moment.unix(item.dt).format("DD MM YYYY")}
+              icon = {`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+              high={item.temp.max}
+              low={item.temp.min}
+              weatherCode={item.weather[0].description}
+              main={item.weather.wind_speed}
+            />)}
            
            
-        </Carousel.Item>
+        </>
             
             )}
         
-        
+       
     </Carousel>
     ))}    
      
