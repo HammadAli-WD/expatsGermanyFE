0c16818773d221e678b4ceecd61e4aba49735f42
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-10-22 23:53:09 +0200

    update

--- expats-app/src/App.js
+++ expats-app/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/Home';
 import SocialNetwork from './components/chat/SocialNetwork';
 import SignUp from './components/SignUp';
+import Login from './components/login/Login';
 
 
 function App(props) {
@@ -13,7 +14,8 @@ function App(props) {
           <Switch>
          <Route exact path= "/" component={Home} />
          <Route path="/join" exact component= {SocialNetwork} />
-         <Route path="/signUp" exact component= {SignUp} />         
+         <Route path="/signUp" exact component= {SignUp} />
+         <Route path="/login" exact component= {Login} />         
        </Switch>
      </Router>   
    
