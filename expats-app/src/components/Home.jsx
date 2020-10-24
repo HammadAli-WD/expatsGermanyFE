4e6ac99abb6fd0ea2865fe18ccc08ec027d2b4eb
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-10-24 02:06:10 +0200

    update

--- expats-app/src/components/Home.jsx
+++ expats-app/src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Covid from "./Covid";
 import Headlines from "./Headlines";
-import Login from "./login/Login"
 import {Container, Row, Col, Jumbotron, Button} from 'react-bootstrap'
 import WeatherInfo from "./weather/WeatherInfo";
 import HackerNews from "./hackerNews/HackerNews";
@@ -15,7 +14,7 @@ function Home() {
         <Container fluid>
         <HackerNews />
         <Row>        
-            <Col sm={3} ><Covid />   </Col>
+            <Col sm={3} ><WeatherInfo /> <br></br><Covid />   </Col>
             <Col sm={6}>  <Jumbotron>
             <h1>Hello, world!</h1>
             <p>
@@ -25,9 +24,9 @@ function Home() {
             <p>
                 <Button variant="primary">Learn more</Button>
             </p>
-            </Jumbotron> <WeatherInfo /> 
+            </Jumbotron>  
                 </Col>
-            <Col sm={3}> <Headlines /> <Login />   </Col>
+            <Col sm={3}> <Headlines /> </Col>
             
         </Row>
         </Container>
