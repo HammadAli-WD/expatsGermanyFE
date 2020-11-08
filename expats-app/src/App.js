1491be659fb016fdab61e2a78b80514e035d847b
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-11-08 02:48:14 +0100

    update

--- expats-app/src/App.js
+++ expats-app/src/App.js
@@ -16,7 +16,7 @@ function App(props) {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/join" exact component={SocialNetwork} />
-          {/* <Route exact path="/rooms/:room" component={props => <Messages room={props.match.params.room} />} /> */}
+          <Route path="/rooms/:room" component={props => <Messages room={props.match.params.room} />} />
           <Route path="/rooms" exact component={ChatRooms} />
           <Route path="/signUp" exact component={SignUp} />
           <Route path="/login" exact component={Login} />
