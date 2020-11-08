e801391077e025ee91c2d7fa87391741076baec4
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-11-08 18:33:57 +0100

    Chat Rooms

--- expats-app/src/components/chat/ChatRooms.jsx
+++ expats-app/src/components/chat/ChatRooms.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, Component } from "react"
 import io from "socket.io-client"
-import { Button, Media, Modal } from "react-bootstrap"
-import styled from "styled-components";
+import { Image, Media, Container } from "react-bootstrap"
+import styled, { keyframes } from "styled-components";
+import { Animated } from "react-animated-css";
+import ParticlesBg from "particles-bg";
 import { Link, withRouter } from 'react-router-dom';
 const connOpt = {
   transports: ["websocket"],
@@ -9,12 +11,12 @@ const connOpt = {
 const { accesstoken } = sessionStorage;
 let socket = io("http://localhost:3005", connOpt)
 
-function ChatRooms(props) {
-  const [username, setUsername] = React.useState(null)
-  const [rooms, setRooms] = React.useState([])
-  const [room, setRoom] = React.useState(null)
 
-  const [showModal, setShowModal] = React.useState(true)
+
+function ChatRooms(props) {
+  const [rooms, setRooms] = React.useState([])
+  const [isVisible, setisVisible] = React.useState(true)
+
   useEffect(() => {
     (async () => {
       try {
@@ -36,35 +38,75 @@ function ChatRooms(props) {
     })()
   }, []);
 
+  let config = {
+    num: [4, 7],
+    rps: 0.1,
+    radius: [2, 40],
+    life: [1.5, 3],
+    v: [2, 3],
+    tha: [-40, 40],
+    alpha: [0.6, 0],
+    scale: [.1, 0.4],
+    position: "all",
+    color: ["random", "#ff0000"],
+    cross: "dead",
+    // emitter: "follow",
+    random: 15
+  };
+
+  if (Math.random() > 0.85) {
+    config = Object.assign(config, {
+      onParticleUpdate: (ctx, particle) => {
+        ctx.beginPath();
+        ctx.rect(
+          particle.p.x,
+          particle.p.y,
+          particle.radius * 2,
+          particle.radius * 2
+        );
+        ctx.fillStyle = particle.color;
+        ctx.fill();
+        ctx.closePath();
+      }
+    });
+  }
 
 
   return (
     <>
+      <ParticlesBg type="custom" config={config} bg={true} />
+      <Animated animationIn="zoomInDown" animationOut="zoomOutDown" animationInDuration={2000} animationOutDuration={1400} isVisible={isVisible}>
+        <div style={{ width: '100vw', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', display: "flex", flexDirection: "column" }}>
+          <div style={{ padding: "10px 5px 15px 20px", textShadow: "1px 1px 0px #ff0000" }}>
+            <h2 style={{ display: "flex", justifyContent: 'center', alignItems: 'center' }} >Welcome</h2>
+            <p>Select the group for social connection with fellow members</p></div>
 
 
 
-      {rooms.map((room, i) => (
+          {rooms.map((room, i) => (
 
-        <Media as="li">
-          <img
-            width={64}
-            height={64}
-            className="mr-3"
-            src={room.image}
-            alt="Generic placeholder"
-          />
+            <Media as="li" style={{ padding: "10px 5px 15px 20px" }}>
+              <Image style={{ height: "200%" }} src={room.image} alt="Generic placeholder" />
 
-          <Media.Body>
 
-            <li key={i} onClick={async () => {
-              await props.history.push(`/rooms/${room.name}`)
-            }}>
-              <strong  >{room.name}</strong>
-            </li>
-          </Media.Body>
-        </Media>
-      ))}
+              <Media.Body style={{ padding: "10px 5px 15px 20px" }}>
 
+                <Link key={i} onClick={async () => {
+                  await setisVisible(false)
+                  await setTimeout(() => props.history.push(`/rooms/${room.name}`), 800);
+
+
+                }}>
+                  <h3 style={{ padding: "0px", margin: "0px" }}>{room.name}</h3>
+                  <p style={{ padding: "0px", margin: "0px", fontStyle: "italic" }}><small>{room.description}</small></p>
+
+                </Link>
+
+              </Media.Body>
+            </Media>
+          ))}
+        </div>
+      </Animated>
     </>
   )
 }
