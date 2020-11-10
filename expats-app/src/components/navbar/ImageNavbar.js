9d20f4874e22005a168f1e3c4bf7916af2e1fdeb
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-11-10 23:52:20 +0100

    CSS adjustment

--- expats-app/src/components/navbar/ImageNavbar.js
+++ expats-app/src/components/navbar/ImageNavbar.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import styled from "styled-components";
+import { Link, withRouter } from 'react-router-dom';
 
 const Image = styled.img`
   height: 35%;
-  margin: auto 0;
+  
+  margin-top: 15px
 `;
 const ImageNavbar = () => {
   return (
-    <Image src="https://inassets1-internationsgmbh.netdna-ssl.com/static/bundles/internationshomepage/frontend/images/whitelogo@3x.png" alt="Company Logo" />
+    <div style={{ maxWidth: '0.5rem' }} >
+      <Link to="/">
+        <Image src="https://inassets1-internationsgmbh.netdna-ssl.com/static/bundles/internationshomepage/frontend/images/whitelogo@3x.png" alt="Company Logo" />
+      </Link>
+    </div>
   )
 }
 
-export default ImageNavbar
+export default withRouter(ImageNavbar)
 
