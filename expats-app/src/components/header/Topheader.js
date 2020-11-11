d7ff41a3aa11c7e6d189a9a856b4550359fa703f
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-11-11 23:11:49 +0100

    media queries

--- expats-app/src/components/header/Topheader.js
+++ expats-app/src/components/header/Topheader.js
@@ -1,25 +1,38 @@
 import React from "react";
-import { Jumbotron} from 'react-bootstrap'
+import { Jumbotron } from 'react-bootstrap'
 import FadeIn from "../../pages/FadeIn";
+import styled from "styled-components";
+
+
+const BurgerWrapper = styled.div`
+  background-image: url(https://www.studying-in-germany.org/wp-content/uploads/2014/02/germany-international-students.jpg) ;
+  background-size: 100% 100%;
+  margin-top: 40px;
+   height: 300px;
+  @media (min-width: 702px) {
+    height: 612px;
+  }
+`;
 
 function Topheader() {
-    
 
     return (
-       <>
-       
-    {/*  https://cdn.prod.www.manager-magazin.de/images/ebc32f7e-0001-0004-0000-000001184842_w718_r1.77_fpx50_fpy55.56.jpg */} 
-    <Jumbotron className="my-5" style={{backgroundImage: 'url(https://www.studying-in-germany.org/wp-content/uploads/2014/02/germany-international-students.jpg)', backgroundSize: "100% 100%", height: '600px'}}>
-        <FadeIn>
-            <h1>Welcome abroad!</h1>
-            <p>
-            Enjoy the international experience with fellow global minds
+        <>
+
+            {/*  https://cdn.prod.www.manager-magazin.de/images/ebc32f7e-0001-0004-0000-000001184842_w718_r1.77_fpx50_fpy55.56.jpg */}
+            <BurgerWrapper>
+
+                <FadeIn>
+                    <h1>Welcome abroad!</h1>
+                    <p>
+                        Enjoy the international experience with fellow global minds
             </p>
-        </FadeIn>            
-    </Jumbotron>
-     
-  
-</>
-)}
+                </FadeIn>
+
+            </BurgerWrapper>
+
+        </>
+    )
+}
 
 export default Topheader
