1d17c42afb47c7ca503e64c2f3abe0b153b65740
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-11-18 15:47:34 +0100

    Fetching from Rapid Api

--- expats-app/src/components/headlines/Headlines.jsx
+++ expats-app/src/components/headlines/Headlines.jsx
@@ -1,46 +1,70 @@
-import React from "react";
-import {Media, Spinner} from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import { Media, Spinner } from 'react-bootstrap'
 import { Link } from "react-router-dom";
-import useFetch from "../../HOC/UseFetch"
 
 function Headlines() {
-    
-  const [response, loading, hasError] = useFetch("http://localhost:3005/news/headlines") 
 
-    return (
-       <>
-       
-    {loading ? 
-    <Spinner animation="border" role="status">
-    <span className="sr-only">Loading...</span>
-  </Spinner>:
-    (hasError?<div>Error...</div>:
-    (
+  const [response, setResponse] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [hasError, setHasError] = useState(false)
 
-      <ul className="list-unstyled">
-        <h2>Bing News Headlines</h2>
-        {response.map((tn,i)=>
-  <Media as="li">
-    <img
-      width={64}
-      height={64}
-      className="mr-3"
-      src={tn.image.url}
-      alt="Generic placeholder"
-    />
-   
-    <Media.Body>
-    <h3><Link to={{ pathname: tn.webSearchUrl }} target="_blank" >{tn.name}</Link></h3>
-    <p>{tn.query.text}</p>
-    </Media.Body>
-  </Media>
- )}
-  
-</ul>
-    ))}    
-     
-  
-</>
-)}
+  useEffect(() => {
+    setLoading(true)
+    fetch(`https://${process.env.REACT_APP_HOST_RAPIDAPI}/news/trendingtopics?textFormat=Raw&safeSearch=Off`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": process.env.REACT_APP_HOST_RAPIDAPI,
+        "x-rapidapi-key": process.env.REACT_APP_KEY_RAPIDAPI,
+        "x-bingapis-sdk": "true"
+      }
+    })
+      .then(response => response.json())
+      .then(data => data.value)
+      .then(slice => {
+        setResponse(slice.slice(0, 5))
+        setLoading(false)
+      })
+      .catch(() => {
+        setHasError(true)
+        setLoading(false)
+      })
+  }, [])
+
+  return (
+    <>
+
+      {loading ?
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner> :
+        (hasError ? <div>Error...</div> :
+          (
+
+            <ul className="list-unstyled">
+              <h2>Bing News Headlines</h2>
+              {response.map((tn, i) =>
+                <Media as="li">
+                  <img
+                    width={64}
+                    height={64}
+                    className="mr-3"
+                    src={tn.image.url}
+                    alt="Generic placeholder"
+                  />
+
+                  <Media.Body>
+                    <h3><Link to={{ pathname: tn.webSearchUrl }} target="_blank" >{tn.name}</Link></h3>
+                    <p>{tn.query.text}</p>
+                  </Media.Body>
+                </Media>
+              )}
+
+            </ul>
+          ))}
+
+
+    </>
+  )
+}
 
 export default Headlines
