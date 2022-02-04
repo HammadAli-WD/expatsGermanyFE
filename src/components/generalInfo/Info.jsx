aba4d67b30e267063baa40baffa2691de3efb3e3
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2022-02-04 11:16:19 +0100

    Update Info.jsx

--- src/components/generalInfo/Info.jsx
+++ src/components/generalInfo/Info.jsx
@@ -96,7 +96,7 @@ function Info() {
          <Col xs={12} md={4}>
             <Card bg="dark" text="white" >
                 <Card.Img style={{ height: "80% !important" }} variant="top"
-                src="https://images.squarespace-cdn.com/content/v1/5df3b3298a8734580f1e0036/1582133993349-L21BGYHF93IFR0QZLC63/ke17ZwdGBToddI8pDm48kLkXF2pIyv_F2eUT9F60jBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iyqMbMesKd95J-X4EagrgU9L3Sa3U8cogeb0tjXbfawd0urKshkc5MgdBeJmALQKw/image-asset.jpeg" />
+                src="https://germany-simplified.com/wp-content/uploads/2020/10/Germany_Simplified_Blog_2WorkVisa.png" />
             <Background>
                 <Card.Header>
                   <Card.Link href="https://service.berlin.de/dienstleistung/325475/en/" target="_blank">
